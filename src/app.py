e7dfa2d46c74586caf580f7cac11ef5e98feff4e
Boris Fritscher <boris@fritscher.ch>
2025-07-25 14:40:40 +0200

    fix health

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ A simple guestbook flask app.
 import flask
 from index import Index
 from sign import Sign
+from health_endpoints import register_health_endpoints
 
 app = flask.Flask(__name__)       # our Flask app
 
@@ -14,6 +15,9 @@ app.add_url_rule('/',
 app.add_url_rule('/sign',
                  view_func=Sign.as_view('sign'),
                  methods=['GET', 'POST'])
+
+# Register health check endpoints
+register_health_endpoints(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
